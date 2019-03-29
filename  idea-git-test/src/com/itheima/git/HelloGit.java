2c59e7e322eab7e4a025a27e73e2a3f510a5353f
yxqs <1084942074@qq.com>
2019-03-29 20:02:05 +0800

    输出警告

---
+++  idea-git-test/src/com/itheima/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.itheima.git;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("涂山苏苏");
+        System.out.println("涂山苏苏");
+        System.out.println("涂山苏苏");
+        System.out.println("涂山苏苏");
+        System.out.println("涂山苏苏");
+        System.out.println("涂山苏苏");
+    }
+}
